0dce4add39501c97cd70ae4d1344f56eba7384ee
Ilker <ilkerfrancisco@gmail.com>
2023-01-27 20:04:22 -0600

    Problema 2 resuelto

---
+++ problema2.py
@@ -0,0 +1,21 @@
+class Problema:
+    def __init__(self):
+        self.primeros = list(range(1,101))
+
+    def extract(self,numero=int()):
+        try:
+            self.primeros.remove(numero)
+            #print(self.primeros)
+        except ValueError:
+            print("el numero ingresado no es un entero",numero)
+
+    def calc(self):
+        numero = set(range(1,101))-set(self.primeros)
+        for i in numero:
+            print(i)
+            break
+p = Problema()
+
+p.extract(numero=3)
+p.calc()
+
